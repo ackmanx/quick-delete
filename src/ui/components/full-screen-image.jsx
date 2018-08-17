4c3ef03fd32b40358d7e9faaef437a798dad5e6d
Eric Majerus <varr@Erics-Mac-mini.local>
2018-08-16 22:15:51 -0500

    deleted all my useless calculations because taylor said to just use max-width/max-height

--- src/ui/components/full-screen-image.jsx
+++ src/ui/components/full-screen-image.jsx
@@ -9,35 +9,11 @@ export class FullScreenImage extends React.Component {
 
     render() {
         const {image} = this.props
-        const style = this.calculateDisplayedImageDimensions(image)
 
         return (
             <div className='full-screen-image'>
-                <img style={style} src={image.srcOriginal}/>
+                <img src={image.srcOriginal}/>
             </div>
         )
-    }
-
-    calculateDisplayedImageDimensions(image) {
-        let displayedImageWidth
-        let displayedImageHeight
-
-        //Landscape
-        if (image.width > image.height) {
-            if (image.width > window.innerWidth) {
-                displayedImageWidth = '100vw'
-            }
-        }
-        //Portrait
-        else {
-            if (image.height > window.innerHeight) {
-                displayedImageHeight = '100vh'
-            }
-        }
-
-        return {
-            width: displayedImageWidth,
-            height: displayedImageHeight,
-        }
     }
 }
