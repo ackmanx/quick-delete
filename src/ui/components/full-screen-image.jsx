99b30d6e84d3684ad269213baf4ae612d4c7874e
Eric Majerus <varr@Erics-Mac-mini.local>
2018-08-16 21:01:12 -0500

    sizing image to always fit in window, but not stretch

--- src/ui/components/full-screen-image.jsx
+++ src/ui/components/full-screen-image.jsx
@@ -9,18 +9,35 @@ export class FullScreenImage extends React.Component {
 
     render() {
         const {image} = this.props
-        let displayedImageWidth
-        let displayedImageHeight
-
-        const style = {
-            width: displayedImageWidth ? displayedImageWidth : undefined,
-            height: displayedImageHeight ? displayedImageHeight : undefined,
-        }
+        const style = this.calculateDisplayedImageDimensions(image)
 
         return (
             <div className='full-screen-image'>
                 <img style={style} src={image.srcOriginal}/>
             </div>
         )
+    }
+
+    calculateDisplayedImageDimensions(image) {
+        let displayedImageWidth
+        let displayedImageHeight
+
+        //Landscape
+        if (image.width > image.height) {
+            if (image.width > window.innerWidth) {
+                displayedImageWidth = window.innerWidth
+            }
+        }
+        //Portrait
+        else {
+            if (image.height > window.innerHeight) {
+                displayedImageHeight = window.innerHeight
+            }
+        }
+
+        return {
+            width: displayedImageWidth,
+            height: displayedImageHeight,
+        }
     }
 }
