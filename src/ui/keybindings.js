7d5a57de5d96a069587dd0b9e36dd137d6569888
Eric M <>
2018-08-18 22:59:29 -0500

    Added space bar shortcut to mark file to delete

--- src/ui/keybindings.js
+++ src/ui/keybindings.js
@@ -1,7 +1,9 @@
 import mousetrap from 'mousetrap'
 import {NEXT_PHOTO, PREVIOUS_PHOTO} from './actions/action-types'
+import markDeleteAction from './actions/mark-delete'
 
 export default function setupKeyBindings(store) {
     mousetrap.bind('left', () => store.dispatch({type: PREVIOUS_PHOTO}))
     mousetrap.bind('right', () => store.dispatch({type: NEXT_PHOTO}))
+    mousetrap.bind('space', () => store.dispatch(markDeleteAction()))
 }
