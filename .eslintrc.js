f08663f6bc57ad69a3612878584b82cfb682ef4f
Eric M <>
2018-08-26 15:00:04 -0500

    added eslint with config

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+//babel-eslint is required for this to work properly, being I use babel for transpiling
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+        jest: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+    ],
+    rules: {
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        'react/no-unused-prop-types': 'error',
+        'linebreak-style': ['error', 'unix'],
+        'no-case-declarations': 'off',
+        'no-console': 'off',
+    }
+}
